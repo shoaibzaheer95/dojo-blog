1ca09b91acf9c0d34bb8f05a6d90240fa63c8fc0
shoaibzaheer95 <shoaibzaheer95@gmail.com>
2023-10-26 18:08:07 +0500

    useEffects and json server

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,14 +1,13 @@
 const BlogList = ({blogs, title}) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
+  
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
             {
             blogs.map((blog)=>(
                 <div className="preview-blog" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p className='author'>written by {blog.author}</p>
+                <h3>{blog.title}</h3>
+                <p className="author">by {blog.author}</p>
                 <p>{blog.body}</p>
                 </div>
             ))
--- src/Home.js
+++ src/Home.js
@@ -1,55 +1,28 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
 
-    // const [name,setName] = useState('mario');
-    // const [age,setAge] = useState(25)
-    // const handleClick = () =>{
-    //    setName('aden');
-    //    setAge(30);
-    // }
-    
-    const [blogs,setBlogs] = useState([   
-        {
-            id: 1,
-            title: 'Introduction to JavaScript',
-            body: 'JavaScript is a versatile programming language used for web development.',
-            author: 'John Doe',
-        },
-        {
-            id: 2,
-            title: 'Working with Arrays',
-            body: 'Arrays are fundamental data structures in JavaScript.',
-            author: 'Jane Smith',
-        },
-        {
-            id: 3,
-            title: 'Building Responsive Web Pages',
-            body: 'Learn how to create responsive web pages with HTML, CSS, and JavaScript.',
-            author: 'David Johnson',
-        },
-        {
-            id: 4,
-            title: 'JavaScript Promises',
-            body: 'Promises help manage asynchronous operations in JavaScript.',
-            author: 'Alice Brown',
-        },
-        {
-            id: 5,
-            title: 'Debugging Techniques',
-            body: 'Debugging is essential for finding and fixing issues in your code.',
-            author: 'Chris Wilson',
-        }
-        ]);
-    
    
+    const [blogs,setBlogs] = useState(null);
+    
+        useEffect(()=>{
+            fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data)
+            })
+            
+        },[]);
     return ( 
 
         <div className="home">
-        
-            <BlogList blogs={blogs} title="All blog!" />
-       
+           
+            {blogs && <BlogList blogs={blogs} title="All blogs!" />}
+
+            {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'Chris Wilson',)} title="Chris Wilson blogs!" handleDelete={handleDelete} /> */}
        
         </div>
      );
